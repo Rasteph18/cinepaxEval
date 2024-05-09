62353888c4020507a154722fabbac701548a3483
Rasteph18 <srandrianarivelo18@gmail.com>
2024-05-09 08:38:47 +0300

    projet initial

---
+++ cinepax/src/test/java/com/projet/cinepax/CinepaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.cinepax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
